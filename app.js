6eab462774d9e86680e0a38df60b22731a528e1a
Even Dalen <even.dalen@live.no>
2017-02-21 09:17:53 +0100

    fixed babel compiler for tests

--- app.js
+++ app.js
@@ -35,38 +35,40 @@ app.use('/api/', apiPost);
 app.use('/api/', apiUser);
 app.use('/api/', apiCompany);
 
-app.get('*', (req, res) => {
-    console.log(req);
-    match(
-        { routes, location: req.url },
-        (err, redirectLocation, renderProps) => {
+if(process.env.NODE_ENV !== 'test') {
+    app.get('*', (req, res) => {
+        console.log(req);
+        match(
+            {routes, location: req.url},
+            (err, redirectLocation, renderProps) => {
 
-            // in case of error display the error message
-            if (err) {
-                return res.status(500).send(err.message);
+                // in case of error display the error message
+                if (err) {
+                    return res.status(500).send(err.message);
+                }
+
+                // in case of redirect propagate the redirect to the browser
+                if (redirectLocation) {
+                    return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+                }
+
+                // generate the React markup for the current route
+                let markup;
+                if (renderProps) {
+                    // if the current route matched we have renderProps
+                    markup = renderToString(<RouterContext {...renderProps}/>);
+                } else {
+                    // otherwise we can render a 404 page
+                    //       markup = renderToString(<NotFoundPage/>);
+                    res.status(404);
+                }
+
+                // render the index template with the embedded React markup
+                return res.render('index', {markup});
             }
-
-            // in case of redirect propagate the redirect to the browser
-            if (redirectLocation) {
-                return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-            }
-
-            // generate the React markup for the current route
-            let markup;
-            if (renderProps) {
-                // if the current route matched we have renderProps
-                markup = renderToString(<RouterContext {...renderProps}/>);
-            } else {
-                // otherwise we can render a 404 page
-                //       markup = renderToString(<NotFoundPage/>);
-                res.status(404);
-            }
-
-            // render the index template with the embedded React markup
-            return res.render('index', { markup });
-        }
-    );
-});
+        );
+    });
+}
 
 
 // catch 404 and forward to error handler
